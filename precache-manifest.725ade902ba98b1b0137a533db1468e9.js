8398395f0154d3d0d7336a1e44f58415d901e170
alexmochu <mochualex4@gmail.com>
2020-01-12 16:26:05 +0300

    Updates

---
+++ precache-manifest.725ade902ba98b1b0137a533db1468e9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c455e3ca96f12ec9c2a4fa36670e7b1",
+    "url": "/profile/index.html"
+  },
+  {
+    "revision": "03839b1c99fb2afdefdc",
+    "url": "/profile/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "b69fdab66aa9bfec1257",
+    "url": "/profile/static/js/2.9c3d3913.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/profile/static/js/2.9c3d3913.chunk.js.LICENSE"
+  },
+  {
+    "revision": "03839b1c99fb2afdefdc",
+    "url": "/profile/static/js/main.4297dea1.chunk.js"
+  },
+  {
+    "revision": "9ea71b400ca9a5d22514",
+    "url": "/profile/static/js/runtime-main.91af3a0e.js"
+  }
+]);
